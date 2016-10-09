ecd7326111332533a4f2a6bfae328b5668e55531
Amar Zeno <Amar Zeno>
2016-10-09 14:52:55 -0600

    Added menu button sound effects

---
+++ Assets/Scripts/ExitButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class ExitButton : MonoBehaviour {
+
+    public void Clicked(string nextSceneName)
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    public void Exit() {
+        Application.Quit();
+    }
+
+    public void OnMouseEnter() {
+        transform.localScale = new Vector3(1.2F, 1.2F, 1.2F);
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        audio.Play(44100);
+    }
+
+    public void OnMouseExit() {
+        transform.localScale = new Vector3(1F, 1F, 1F);
+    }
+}
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -2,6 +2,7 @@
 using UnityEngine.SceneManagement;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
 public class StartButton : MonoBehaviour {
 
     public void Clicked(string nextSceneName)
@@ -11,5 +12,16 @@ public class StartButton : MonoBehaviour {
 
     public void Exit() {
         Application.Quit();
+    }
+
+    public void OnMouseEnter() {
+        transform.localScale = new Vector3(1.2F, 1.2F, 1.2F);
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+        audio.Play(44100);
+    }
+
+    public void OnMouseExit() {
+        transform.localScale = new Vector3(1F, 1F, 1F);
     }
 }
